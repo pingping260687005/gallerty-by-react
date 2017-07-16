fed9bfb80f719225972c16cf1c5c12dee3dfa3f6
unknown <>
2017-07-16 18:47:07 +0800

    ddddd

---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+//let yeomanImage = require('../images/yeoman.png');
+// 获得图片相关信息
+let imageDatas = require("../data/imageDatas.json");
+
+// 利用自执行函数， 将图片信息转成url路径
+imageDatas = (function(imageArray){
+  for(var i=0;i <imageArray.length; i++)
+  {
+    var singleImageData = imageArray[i];
+    singleImageData.imageURL = require("../images/"+singleImageData.fileName);
+    imageArray[i] = singleImageData;
+  }
+  return imageArray;
+})(imageDatas);
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="img-sec">    
+        </section>
+         <nav className="controller-nav"></nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+ReactDOM.render(
+  <AppComponent />,
+  document.getElementById('app')
+);
+
+export default AppComponent;
